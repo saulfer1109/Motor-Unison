b00d21df766c90e7578f7f9078697b3116977545
OrlandoCvs <112524194+OrlandoCvs@users.noreply.github.com>
2024-12-05 18:04:24 -0600

    Add files via upload

---
+++ Motores-Unison-App/Views/InicioView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Notas_Unison.Views;
+
+public partial class InicioView : Page
+{
+    public InicioView()
+    {
+        InitializeComponent();
+    }
+}
